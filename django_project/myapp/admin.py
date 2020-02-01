e1ea39fd8eb2da8ec8b90c8a55b6bfc6986e6e5e
gmshaik <gmshaik.kw@gmail.com>
2020-02-01 14:17:18 +0300

    Notes

--- django_project/myapp/admin.py
+++
@@ -1,5 +0,0 @@
-# from django.contrib import admin
-# from myapp.models import Post
-#
-# # Register your models here.
-# admin.site.register(Post)
--- helloworld/helloworld_project/pages/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-# Create your views here.
-
-def homepageview(request):
-    return HttpResponse("Hello World!")
